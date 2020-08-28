d422b465ba81160f5df1f2d64cfb84ae83aedfd1
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-28 13:24:50 +0200

    Clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-ls -al
-pwd
+# Switch to working directory
 cd /gource
 
 # Start Xvfb
 printf "> Starting Xvfb "
-Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &> ./logs/Xvfb.log &
+Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &> ./logs/xvfb.log &
 xvfb_pid="$!"
 
 # possible race condition waiting for Xvfb.
 sleep 5
-printf "\r\t\t\t\t\t\t\t\t\t\t\t\t\t✔\n"
 
 # Check if git repo exist, else download it from GIT_URL
 printf "\n>\n> Repository check \n"
 if [ -z "$(ls -A ./git_repos)" ]; then
-    printf "> \tUsing single repo \n"
+  printf "> \tUsing single repo \n"
 
 	# Check if git repo needs to be cloned
   if [ -z "$(ls -A ./git_repo)" ]; then
@@ -28,12 +26,12 @@ if [ -z "$(ls -A ./git_repos)" ]; then
     else
       # Check if git repo need token
       if [ "${GIT_TOKEN}" == "" ]; then
-          printf "> \tCloning from public: ${GIT_URL}"
-          timeout 25s git clone ${GIT_URL} ./git_repo >/dev/null 2>&1
+        printf "> \tCloning from public: ${GIT_URL}"
+        timeout 25s git clone ${GIT_URL} ./git_repo >/dev/null 2>&1
       else
-          printf "> \tCloning from private: ${GIT_URL}"
-          # Add git token to access private repository
-          timeout 25s git clone $(sed "s/git/${GIT_TOKEN}\@&/g" <<< ${GIT_URL}) ./git_repo >/dev/null 2>&1
+        printf "> \tCloning from private: ${GIT_URL}"
+        # Add git token to access private repository
+        timeout 25s git clone $(sed "s/git/${GIT_TOKEN}\@&/g" <<< ${GIT_URL}) ./git_repo >/dev/null 2>&1
       fi
     fi
   fi
@@ -48,26 +46,26 @@ if [ -z "$(ls -A ./git_repos)" ]; then
 	printf "\n> \tUsing volume mounted git repo"
 	gource --output-custom-log ./development.log ./git_repo >/dev/null 2>&1
 else
-    # // TODO: Add multi repo support
+  # // TODO: Add multi repo support
 	printf "\n\nERROR: Currently multiple repos are not supported"
-    exit 1
+  exit 1
 fi
 
 
 # Set proper env variables if we have a logo.
 printf "\n>\n> Logo check"
 if [ "${LOGO_URL}" != "" ]; then
-    printf "\n> \tDownloading logo"
+  printf "\n> \tDownloading logo"
 	wget -O ./logo.image ${LOGO_URL} >/dev/null 2>&1
-    convert -geometry x160 ./logo.image ./logo.image
+  convert -geometry x160 ./logo.image ./logo.image
 
-    printf "\n> \tUsing logo from: ${LOGO_URL} \n"
-    export LOGO=" -i ./logo.image "
-    export LOGO_FILTER_GRAPH=";[with_date][2:v]overlay=main_w-overlay_w-40:main_h-overlay_h-40[with_logo]"
-    export FILTER_GRAPH_MAP=" -map [with_logo] "
+  printf "\n> \tUsing logo from: ${LOGO_URL} \n"
+  export LOGO=" -i ./logo.image "
+  export LOGO_FILTER_GRAPH=";[with_date][2:v]overlay=main_w-overlay_w-40:main_h-overlay_h-40[with_logo]"
+  export FILTER_GRAPH_MAP=" -map [with_logo] "
 else
-    printf "\n> \tNo logo provided, skipping logo setup\n"
-    export FILTER_GRAPH_MAP=" -map [with_date] "
+  printf "\n> \tNo logo provided, skipping logo setup\n"
+  export FILTER_GRAPH_MAP=" -map [with_date] "
 fi
 
 # Run the visualization
@@ -76,11 +74,14 @@ printf ">\n> Starting gource script\n"
 printf "\n> Gource script completed"
 
 # Copy logs and output file to mounted directory
-printf "\n>\n> Copy data into mounted directory"
-printf "\n> \tCopy generated mp4"
-cp ./output/gource.mp4 /github/workspace/gource.mp4
-printf "\n> \tCopy log files"
-cp -r ./logs/ /github/workspace/
+printf "\n>\n>Clean up"
+printf "\n> \tCopy generated mp4 to /github/workspace/gource"
+cp ./output/gource.mp4 /github/workspace/gource/gource.mp4
+printf "\n> \tCopy logs to /github/workspace/gource/logs"
+cp -r ./logs/ /github/workspace/gource
+printf "\n> \tDelete working directory"
+rm -r /gource
+
 
 # Exit
 printf "\n>\n> Done.\n>"

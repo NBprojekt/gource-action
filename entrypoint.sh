92ed3776abaf45d41c850ebf2bca769bcae465b4
Norbert Bartko <bartko.norbert@gmail.com>
2020-08-28 12:19:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [ -z "$(ls -A ./git_repos)" ]; then
 	# Check if git repo needs to be cloned
   if [ -z "$(ls -A ./git_repo)" ]; then
     # Check if GIT_URL is a local folder
-    if [ "${GIT_URL}" =~ [^\.\/] ]; then
+    if [ "${GIT_URL}" != $(sed "s/.\//\/github\/workflow\@&/g" <<< ${GIT_URL}) ]; then
       printf "> \tUsing local repository: ${GIT_URL}"
       cp -rf $(sed "s/.\//\/github\/workflow\@&/g" <<< ${GIT_URL}) ./git_repo
     else

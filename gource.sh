7593f04c6b3ef477263fe80198ac54c222d1cb73
Norbert Bartko <bartko.norbert@gmail.com>
2020-09-07 17:54:14 +0200

    Still trying to figure the optional param out

--- gource.sh
+++ gource.sh
@@ -89,7 +89,7 @@ gource --seconds-per-day ${INPUT_GOURCE_SECONDS_PER_DAY} \
 	--bloom-multiplier 1.2 \
 	--${GOURCE_RES} \
 	--stop-at-end \
-	./development.log ${optionalParams[@]} \
+	./development.log "${optionalParams[@]}" \
 	-r ${INPUT_GOURCE_FPS} \
 	-o - >./tmp/gource.pipe &
 
@@ -110,7 +110,7 @@ gource --seconds-per-day ${INPUT_GOURCE_SECONDS_PER_DAY} \
 	--dir-name-depth 3 \
 	--filename-time 2 \
 	--max-user-speed 500 \
-	./development.log ${optionalParams[@]} \
+	./development.log "${optionalParams[@]}" \
 	-r ${INPUT_GOURCE_FPS} \
 	-o - >./tmp/overlay.pipe &
 
